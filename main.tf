c5c77b853df2c5c74fb85a0c25f5d271deb5880d
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2023-02-05 20:04:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
-resource "google_project_iam_member" "member" {
-  project = "basic-tube-373302"
-  role    = "roles/storage.objectViewer"
-  member  = "user:buddhiprakash19evjcs@vitj.ac.in"
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/iam.serviceAccountUser"
+
+    members = [
+      "user:buddhiprakash19evjcs022@vitj.ac.in",
+    ]
+  }
+}
+
+resource "google_service_account" "sa" {
+  account_id   = "myacc"
+  display_name = "bpacc"
+}
+
+resource "google_service_account_iam_policy" "admin-account-iam" {
+  service_account_id = google_service_account.sa.name
+  policy_data        = data.google_iam_policy.admin.policy_data
 }
